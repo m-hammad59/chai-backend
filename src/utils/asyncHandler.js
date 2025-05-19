f06b0d8d4963044a7374e50fa6b6bc1a0f6c766b
hammad <developergymer@gmail.com>
2025-05-19 18:58:35 +0500

    jwt_1

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+
+// First approch for Promises and catch with higher-order function
+
+const asyncHandler = (requestHandler) => {
+
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+
+}
+
+export {asyncHandler}
+
+
+
+// second approch for try catch with higher-order function
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next) 
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
