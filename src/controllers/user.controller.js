c33ad55edd5fa4b431971084366f45de8e689c10
hammad <developergymer@gmail.com>
2025-06-02 19:32:15 +0500

    postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,27 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
 
 const registerUser = asyncHandler(async(req, res) => {
-    res.status(200).json({
-        message: "ok"
+  const { fullName, email, username, password }  = req.body
+
+  console.log(email);
+
+  if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+        throw new ApiError(400, "All Fields Required")
+  }
+  
+    //await added by gpt   
+   const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
+
+    if (existedUser) {
+        throw new ApiError(409, "Username or Email already exist")
+    }
+
 })
 
 export {registerUser}
