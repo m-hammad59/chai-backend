d2e55711c46c2c35c28d18f2b9b724bb368d1efa
hammad <developergymer@gmail.com>
2025-06-10 21:38:02 +0500

    registeration controller postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,7 +9,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async(req, res) => {
   const { fullName, email, username, password }  = req.body
 
-  console.log(email);
+  // console.log(email);
 
   if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -26,8 +26,18 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new ApiError(409, "Username or Email already exist")
     }
 
+    // console.log(req.files);
+    
+
    const avatarLocalPath = req.files?.avatar[0]?.path
-   const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+  //  const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+  let coverImageLocalPath;
+
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
 
    if (!avatarLocalPath) {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,8 +19,10 @@ cloudinary.config({
             } )
             // file has been uploaded successfully
 
-            console.log("File is uploaded on cloudinary", response.url);
+            // console.log("File is uploaded on cloudinary", response.url);
             
+            fs.unlinkSync(localFilePath)
+
             return response
 
         } catch (error) {
