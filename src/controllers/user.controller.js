3f1eed593165beb46942e6d0fcc3470d80aca2b3
hammad <developergymer@gmail.com>
2025-07-01 19:13:14 +0500

    JWT Access and refresh token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefereshToken = async(userId) => {
 
@@ -163,9 +163,56 @@ const loginUser = asyncHandler(async (req, res) => {
   })
 
 
+  const refreshAccessToken =  asyncHandler(async(req, res) => {
+     
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+      throw new ApiError(401, "Unauthoirzed Request")
+    }
+
+    try {
+      const decodedToken = jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+      )
+  
+      const user = await User.findById(decodedToken?._id)
+  
+      if (!user) {
+        throw new ApiError(401, "Invalid Refresh Token")
+      }
+  
+      if (incomingRefreshToken !== user?.refreshToken) {
+          throw new ApiError(401, "Refresh token is expred or used")
+      }
+  
+        const options = {
+          httpOnly: true,
+          secure: true
+        }
+  
+        const {accessToken, newRefreshToken}= await generateAccessAndRefereshToken(user._id)
+  
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)      
+        .cookie("refreshToken", newRefreshToken, options)      
+        .json(
+          new ApiResponse(
+            200,
+            {accessToken, refreshToken: newRefreshToken},
+            "Access Token Refreshed SuccessFully"
+          )
+        )
+    } catch (error) {
+      throw new ApiError(401, error?.message || "invalid refresh token")
+    }
+  })
 
 export {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  refreshAccessToken
 }
